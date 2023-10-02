69c4afba915d3118908bf137db4547b5ea30503e
Gia Huy <ngogiahuy20012004@gmail.com>
2023-10-02 17:44:59 +0700

    Add files via upload

---
+++ src/Subtract/color/opencv_subtract.cpp
@@ -0,0 +1,35 @@
+#include "opencv2/opencv.hpp"
+#include <opencv2/cudaarithm.hpp>
+
+cv::Mat opencv_subtract(const cv::Mat &img1, const cv::Mat &img2)
+{
+    cv::cuda::GpuMat d_img1, d_img2, d_result;
+
+    d_img1.upload(img1);
+    d_img2.upload(img2);
+
+    cv::cuda::subtract(d_img1, d_img2, d_result);
+
+    cv::Mat result;
+    d_result.download(result);
+
+    return result;
+}
+
+int main()
+{
+    cv::Mat img1 = cv::imread("1.jpg");
+    cv::Mat img2 = cv::imread("2.jpeg");
+
+    // Resize images to the same size if needed
+    cv::Size newSize(500, 500);
+    cv::resize(img1, img1, newSize);
+    cv::resize(img2, img2, newSize);
+
+    cv::Mat result = opencv_subtract(img1, img2);
+
+    cv::imshow("Result", result);
+    cv::waitKey();
+
+    return 0;
+}
